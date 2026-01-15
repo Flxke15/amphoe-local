370c8db88dd54a11b27295ea00bde8e1f99f4393
CHANATHIP <chanathip.c@cdg.co.th>
2026-01-15 14:01:07 +0700

    feat: add Autocomplete and FontSizeController components with type definitions; create auto-imports and typed-router files for improved component usage

--- components.d.ts
+++ components.d.ts
@@ -13,6 +13,8 @@ declare module 'vue' {
   export interface GlobalComponents {
     AppBar: typeof import('./src/components/AppBar.vue')['default']
     AppFooter: typeof import('./src/components/AppFooter.vue')['default']
+    Autocomplete: typeof import('./src/components/Autocomplete.vue')['default']
+    FontSizeController: typeof import('./src/components/FontSizeController.vue')['default']
     HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
     Loading: typeof import('./src/components/Loading.vue')['default']
     MapShow: typeof import('./src/components/MapShow.vue')['default']
